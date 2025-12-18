e3f86dfb35e153bd4591165e596c57107d2ef759
frajavimopu <frajavimopu@gmail.com>
2025-12-18 06:37:03 -0600

    Add sum logic, controller and unit test

--- src/test/java/com/fjmp/devops_calculator/DevopsCalculatorApplicationTests.java
+++ src/test/java/com/fjmp/devops_calculator/DevopsCalculatorApplicationTests.java
@@ -1,13 +1,19 @@
 package com.fjmp.devops_calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevopsCalculatorApplicationTests {
+	private Calculator calculator = new Calculator();
 
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
+	}
 }
