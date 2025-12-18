0fd76cf4f6323f6dd4cbd9659d086b1e5872fdf0
frajavimopu <frajavimopu@gmail.com>
2025-12-17 23:22:59 -0600

    Add Spring Boot skeleton

---
+++ src/main/java/com/fjmp/devops_calculator/DevopsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.fjmp.devops_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjmp/devops_calculator/DevopsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjmp.devops_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
