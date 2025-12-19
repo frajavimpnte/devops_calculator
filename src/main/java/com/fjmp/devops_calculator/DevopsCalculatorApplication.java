af1d150da86971183e665125f9fc276009d6f095
frajavimopu <frajavimopu@gmail.com>
2025-12-19 07:10:18 -0600

    Add static code analysis reports

--- src/main/java/com/fjmp/devops_calculator/DevopsCalculatorApplication.java
+++ src/main/java/com/fjmp/devops_calculator/DevopsCalculatorApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevopsCalculatorApplication {
-
+	//private static final String constant = "constant";
+	private static final String CONSTANT = "constant";
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsCalculatorApplication.class, args);
 	}
